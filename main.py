1f585c892e2bc9d10b3e9265d4aed7b7eb2e059f
Mr-afk-cell <kboxser2026@kehillahstudent.org>
2025-04-01 10:25:54 -0700

    submitted

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import random
 # Write your code here
+#show 5 flashcards in random order
+flashcards = {
+"What is your favourite color?": "Green",
+"What sport do you play?": "Tennis",
+"How old are you?" : 75,
+"What school do you go to?" : "Kehillah",
+"What is your favourite food?" : "Pizza"
+}
+correct_answers = 0 
+questions = list(flashcards.keys() )
+random_questions = random.sample(questions, k= 5)
+for question in random_questions:
+    answer = input(question)
+    if answer == flashcards[question]: 
+        print("correct")
+        correct_answers = correct_answers + 1
+
+if correct_answers >= 3:
+    print("good job")
+else: 
+    print("better luck next time")
